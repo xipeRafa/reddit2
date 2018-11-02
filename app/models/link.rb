49a59ff851e19061e74b390081a22b40e0580ef8
Rafa <rafa@pop-os.localdomain>
2018-11-02 03:06:14 -0700

    fix render name and add validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
+	validates_format_of :url, :with => URI::regexp(%w(http https))
+	validates :title, presence: true, length: { minimum: 5 }
+
     acts_as_votable
 	belongs_to :user
 	has_many :comments
